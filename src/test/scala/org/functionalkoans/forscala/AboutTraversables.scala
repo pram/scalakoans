637740778a4facea0c03909c6183250d6020a0a0
Pram Attale <pram@attale.com>
2015-10-08 18:44:24 +0100

    about traversables

--- src/test/scala/org/functionalkoans/forscala/AboutTraversables.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTraversables.scala
@@ -15,10 +15,10 @@ class AboutTraversables extends KoanSuite with ShouldMatchers {
     val set = Set(1, 9, 10, 22)
     val list = List(3, 4, 5, 10)
     val result = set ++ list
-    result.size should be(__)
+    result.size should be(7)
 
     val result2 = list ++ set
-    result2.size should be(__)
+    result2.size should be(8)
   }
 
   koan( """map will apply the given function on all elements of a
